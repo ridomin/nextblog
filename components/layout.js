47666c0dcb061db9cfc4aa0054c794f45734e710
ridomin <ridomin@live.com>
2020-07-29 00:23:52 -0700

    fix site name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Rido'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Rido Website'
 
 export default function Layout ({ children, home }) {
   return (
