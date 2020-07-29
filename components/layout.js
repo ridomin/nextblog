1e6d0b12bf88a2c4755525275b94d595732e3614
ridomin <ridomin@live.com>
2020-07-29 08:59:19 -0700

    add google analytics

--- components/layout.js
+++ components/layout.js
@@ -2,11 +2,19 @@ import Head from 'next/head'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import { initGA, logPageView } from './googleAnalytics.js'
 
 const name = 'Rido'
 export const siteTitle = 'Rido Website'
 
 export default function Layout ({ children, home }) {
+  if (typeof window !== 'undefined') {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+  }
+  logPageView()
   return (
     <div className={styles.container}>
       <Head>
@@ -14,12 +22,6 @@ export default function Layout ({ children, home }) {
         <meta
           name='description'
           content='Learn how to build a personal website using Next.js'
-        />
-        <meta
-          property='og:image'
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name='og:title' content={siteTitle} />
         <meta name='twitter:card' content='summary_large_image' />
